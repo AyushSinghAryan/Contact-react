d04e46e824fe3117028931397024c2663dbdf342
Ayush Singh Aryan <ayushsingharyan@Ayushs-MacBook-Air.local>
2023-03-11 22:05:40 +0530

    A NEW file is added

---
+++ src/components/AddContact.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class AddContact extends React.Component{
+    // it is  a class components
+    render(){
+        return(
+            <div className = "ui main">
+            <h2>AddContact</h2>
+            <form className="ui form">
+                <div className="filed">
+                    <label>Name</label>
+                </div>
+            </form>
+// continue 16:50
+
+            </div>
+        );
+    }
+}
